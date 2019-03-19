5e5bd9a5ff596f2d3ba7556cd165034a512a8a47
zmbslyr <mhedgeland@gmail.com>
2019-03-18 21:32:25 -0400

    Update main.c

--- main.c
+++ main.c
@@ -11,8 +11,9 @@ int main(void)
 	int i = 6;
 	_printf("This is a string %c\n", 'h');
 	_printf("This is also a sting\n");
-	_puts("String li\"teral");
 	_printf("That's a %s!\n", "penis");
-	_printf("%s", _itoa(i, buffer, 10));
+
+
+	_printf("%d\n", 4);
 	return (0);
 }
