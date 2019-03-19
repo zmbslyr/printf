d3793ee9f3ed0cfaca1d22141176523208c3c457
OrangeB0lt <667@holbertonschool.com>
2019-03-18 21:25:03 -0400

    resubmitting

--- holberton.h
+++ holberton.h
@@ -6,8 +6,6 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-#define BufLen 1024
-
 typedef struct ftoFunc
 {
 	char *form;
@@ -23,9 +21,12 @@ int modForm(int n);
 int _strlen(char *s);
 void _puts(char *str);
 char *_strcpy(char *dest, char *src);
-void _swap(int *a, int *b);
+void _swap(char *a, char *b);
 int _strcmp(const char *s1, const char *s2);
 int (*getForm(const char *s))(va_list c);
+char *_reverse(char *buffer, int i, int j);
+int _abs(int n);
+char *_itoa(int value, char* buffer, int base);
 
 
 #endif
